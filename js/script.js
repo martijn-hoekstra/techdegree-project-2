2e6bf680bc296909fadbc3dc4efa62da1a298315
Martijn Hoekstra <martijn_129@icloud.com>
2019-02-27 19:29:08 +0100

    Added pagination for search results

--- js/script.js
+++ js/script.js
@@ -1,27 +1,23 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
 /***
-   Add your global variables that store the DOM elements you will
-   need to reference and/or manipulate.
-
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
+   Comment
 ***/
 
 const container = document.querySelector('.page');
 const ul = document.querySelector('.student-list');
 const list = ul.children;
+
+let div = document.createElement('div');
+let pagination = document.createElement('ul');
+pagination.className = 'pagination';
+div.appendChild(pagination);
+container.appendChild(div);
+
 let amountPerPage = 10;
-let amountOfPages = Math.ceil(list.length / amountPerPage);
+
+/***
+   Comment
+***/
+
 const searchStudents = (list) => {
   const pageHeader = container.firstElementChild;
   const studentSearchDiv = document.createElement('div');
@@ -36,38 +32,31 @@ const searchStudents = (list) => {
 
   const filterList = () => {
     let searchValue = searchInput.value.toLowerCase();
+
     for(let i = 0; i < list.length; i++) {
       let student = list[i];
       let studentName = student.querySelector('h3').textContent.toLowerCase();
       let studentEmail = student.querySelector('.email').textContent.toLowerCase();
 
       if(studentName.includes(searchValue) || studentEmail.includes(searchValue)) {
-        console.log(studentName.search(searchValue));
-        list[i].style.display = '';
+        //console.log(studentName.search(searchValue));
+        list[i].style.display = 'block';
       } else {
         list[i].style.display = 'none';
       }
     }
+    let searchResult = ul.querySelectorAll('li[style*="display: block;"]');
+    //console.log(searchResult);
+    showPage(searchResult, 1);
   };
 
   searchInput.addEventListener('keyup', filterList);
   searchButton.addEventListener('click', filterList);
-
 };
-searchStudents(list);
-/***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
 
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
+
+/***
+   Comment
 ***/
 
 const showPage = (list, page) => {
@@ -81,57 +70,39 @@ const showPage = (list, page) => {
         list[i].style.display = 'none';
       }
     }
+    appendPageLinks(list);
 };
-showPage(list, 1);
+
+
 /***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
+   Comment
 ***/
 
 const appendPageLinks = (list) => {
-  /*
-  1. ​Determine how many pages are needed for the list by dividing the
-  ​total number of list items by the max number of items per page
-  2. ​Create a d​iv,​ give it the “pagination” class, and append it to the .page div
-  3. ​Add a u​ l​ to the “pagination” div to store the pagination links
-  4. ​for​ every page, add l​i​ and ​a​ tags with the page number text
-  5. ​Add an event listener to each a​ tag. When they are clicked
-  ​call the showPage function to display the appropriate page
-  6. Loop over pagination links to remove active class from all links
-  7. Add the active class to the link that was just clicked. You can identify that
-  clicked link using ​event.target ​
-  */
-  let div = document.createElement('div');
-  let ul = document.createElement('ul');
-  div.className = 'pagination';
-  div.appendChild(ul);
-  container.appendChild(div);
+  pagination.innerHTML = '';
+  let amountOfPages = Math.ceil(list.length / amountPerPage);
 
-  for(let i = 0; i < amountOfPages; i++) {
-    let li = document.createElement('li');
-    let link = document.createElement('a');
-    link.textContent = i + 1;
-    li.appendChild(link);
-    ul.appendChild(li);
-  }
-
-  ul.addEventListener('click', (e) => {
-    const element = e.target;
-    if(e.target.tagName === 'A') {
-      const allLinks = ul.querySelectorAll('a');
-      for(let i = 0; i < allLinks.length; i++) {
-        allLinks[i].className = '';
-      }
-      showPage(list, e.target.textContent);
-      e.target.className = 'active';
+  if(!(list.length < 10)) {
+    for(let i = 0; i < amountOfPages; i++) {
+      let li = document.createElement('li');
+      let link = document.createElement('a');
+      link.textContent = i + 1;
+      li.appendChild(link);
+      pagination.appendChild(li);
     }
-  });
+    pagination.addEventListener('click', (e) => {
+      const element = e.target;
+      if(e.target.tagName === 'A') {
+        const allLinks = pagination.querySelectorAll('a');
+        for(let i = 0; i < allLinks.length; i++) {
+          allLinks[i].className = '';
+        }
+        showPage(list, e.target.textContent);
+        e.target.className = 'active';
+      }
+    });
+  }
 };
-appendPageLinks(list);
 
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+showPage(list, 1);
+searchStudents(list);
