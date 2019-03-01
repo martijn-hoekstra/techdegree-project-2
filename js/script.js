517a057ec3f5c4e007df70b66cc313efcd9bda73
Martijn Hoekstra <martijn_129@icloud.com>
2019-03-01 21:23:34 +0100

    Refactored code

--- js/script.js
+++ js/script.js
@@ -1,24 +1,28 @@
 /***
-   Comment
+   Select and create important elements
 ***/
 
 const container = document.querySelector('.page');
-const ul = document.querySelector('.student-list');
-const list = ul.children;
+const ul = container.querySelector('.student-list');
+const listOfStudents = ul.children;
 
-let div = document.createElement('div');
-let pagination = document.createElement('ul');
+const div = document.createElement('div');
+const pagination = document.createElement('ul');
 pagination.className = 'pagination';
 div.appendChild(pagination);
 container.appendChild(div);
 
+// Sets the amount of students displayed on one page
 let amountPerPage = 10;
 
+
 /***
-   Comment
+   Creates search input for users to search through students and
+   display only matching results on the page
 ***/
 
-const searchStudents = (list) => {
+const searchStudents = list => {
+  // Create and append search input and search button
   const pageHeader = container.firstElementChild;
   const studentSearchDiv = document.createElement('div');
   studentSearchDiv.className = 'student-search';
@@ -30,24 +34,36 @@ const searchStudents = (list) => {
   studentSearchDiv.appendChild(searchButton);
   pageHeader.appendChild(studentSearchDiv);
 
+  // Filters students
   const filterList = () => {
-    let searchValue = searchInput.value.toLowerCase();
+    let students = [];
+    const searchValue = searchInput.value.toLowerCase();
+    const listLength = list.length;
 
-    for(let i = 0; i < list.length; i++) {
-      let student = list[i];
-      let studentName = student.querySelector('h3').textContent.toLowerCase();
-      let studentEmail = student.querySelector('.email').textContent.toLowerCase();
+    for(let i = 0; i < listLength; i++) {
+      const student = list[i];
+      const studentName = student.querySelector('h3')
+                        .textContent
+                        .toLowerCase();
+      const studentMail = student.querySelector('.email')
+                        .textContent
+                        .toLowerCase();
 
-      if(studentName.includes(searchValue) || studentEmail.includes(searchValue)) {
-        //console.log(studentName.search(searchValue));
-        list[i].style.display = 'block';
-      } else {
-        list[i].style.display = 'none';
+      if(studentName.includes(searchValue) || studentMail.includes(searchValue)) {
+        students.push(student);
       }
     }
-    let searchResult = ul.querySelectorAll('li[style*="display: block;"]');
-    //console.log(searchResult);
-    showPage(searchResult, 1);
+    // Hide all students
+    hideAllStudents(listOfStudents);
+
+    // Display students on page
+    let page = showPage(students, 1);
+    if(page) {
+      for(let i = 0; i < page.length; i++) {
+        page[i].style.display = '';
+      }
+      appendPageLinks(students);
+    }
   };
 
   searchInput.addEventListener('keyup', filterList);
@@ -56,53 +72,90 @@ const searchStudents = (list) => {
 
 
 /***
-   Comment
+   Return an array of students that needs to be displayed on the active page
 ***/
 
-const showPage = (list, page) => {
-    let lastItem = page * amountPerPage;
-    let firstItem = lastItem - amountPerPage;
+const showPage = (list, page = 1) => {
+    const lastItem = page * amountPerPage;
+    const firstItem = lastItem - amountPerPage;
+    const listLength = list.length;
+    let students = [];
+
     // Loop over items in the list parameter
-    for(let i = 0; i < list.length; i += 1) {
-      if(i >= firstItem && i < lastItem) {
-        list[i].style.display = '';
-      } else {
-        list[i].style.display = 'none';
+    if(listLength) {
+      for(let i = 0; i < listLength; i++) {
+        if(i >= firstItem && i < lastItem) {
+          students.push(list[i]);
+        }
       }
+      return students;
+    } else {
+      alert('test');
     }
-    appendPageLinks(list);
+};
+
+// Hide all students
+const hideAllStudents = list => {
+  for(let i = 0; i < list.length; i++) {
+    list[i].style.display = 'none';
+  }
 };
 
 
 /***
-   Comment
+   Create pagination buttons where every button will listen
+   trigger a click event and loads a new page of students based
+   on the page number that's been clicked.
 ***/
 
-const appendPageLinks = (list) => {
+const appendPageLinks = list => {
   pagination.innerHTML = '';
-  let amountOfPages = Math.ceil(list.length / amountPerPage);
+  const amountOfPages = Math.ceil(list.length / amountPerPage);
+  const listLength = list.length;
 
-  if(!(list.length < 10)) {
+  if(listLength > amountPerPage) {
+    // Create and append pagination buttons
     for(let i = 0; i < amountOfPages; i++) {
-      let li = document.createElement('li');
-      let link = document.createElement('a');
+      const li = document.createElement('li');
+      const link = document.createElement('a');
       link.textContent = i + 1;
+      link.style.cursor = 'pointer';
       li.appendChild(link);
       pagination.appendChild(li);
     }
+
+    let page = showPage(list, 1);
+    for(let i = 0; i < page.length; i++) {
+      page[i].style.display = '';
+    }
+
+    const allLinks = pagination.getElementsByTagName('a');
+    const allLinksLength = allLinks.length;
+
+    // Add click event listener to every pagination button
     pagination.addEventListener('click', (e) => {
       const element = e.target;
+      const pageNumber = e.target.textContent;
       if(e.target.tagName === 'A') {
-        const allLinks = pagination.querySelectorAll('a');
-        for(let i = 0; i < allLinks.length; i++) {
+        // Remove all 'active' classes on pagination buttons
+        for(let i = 0; i < allLinksLength; i++) {
           allLinks[i].className = '';
         }
-        showPage(list, e.target.textContent);
+
+        // Hide all students
+        hideAllStudents(listOfStudents);
+
+        // Display students on page
+        let page = showPage(list, pageNumber);
+        for(let i = 0; i < page.length; i++) {
+          page[i].style.display = '';
+        }
         e.target.className = 'active';
       }
     });
   }
 };
 
-showPage(list, 1);
-searchStudents(list);
+hideAllStudents(listOfStudents);
+appendPageLinks(listOfStudents);
+searchStudents(listOfStudents);
